086618cf43e817d83eca4a7628933c60cdc027d5
clairecassidy <clairecassidy0@hotmail.com>
2020-12-02 12:27:27 +0000

    client can display server responses

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 
 import axios from 'axios';
+import { useState } from 'react'; 
 
 function App() {
   console.log("Log");
+
+  const [serverResponses, setServerResponses] = useState([]);
 
   const hitBackend = () => {
     axios.get('/test')
       .then((response) => {
         console.log(response.data);
+        setServerResponses((serverResponses) => [...serverResponses, response.data]);
       });
   }
 
@@ -15,6 +19,14 @@ function App() {
     <>
       <h1>Hello World!</h1>
       <button onClick={hitBackend}>Send request</button>
+      {serverResponses.map((response, index) => {
+        return(
+          <>
+            <p>{index}</p>
+            <p>{response}</p>
+          </>
+        )
+      })}
     </>
   );
 }
