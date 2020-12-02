70295a152fdd0d0976fa6f7e56fbab29596becb9
clairecassidy <clairecassidy0@hotmail.com>
2020-12-02 12:56:14 +0000

    working stock request to API on client side

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,18 @@ function App() {
       });
   }
 
+  const sendReqClienSide = () => {
+    axios.get('https://api.github.com/search/users?q=esjmb')
+    .then((response) => {
+      console.log(response.data);
+    });
+  }
+
   return (
     <>
       <h1>Hello World!</h1>
       <button onClick={hitBackend}>Send request</button>
+      <button onClick={sendReqClienSide}>Log Stephen's info</button>
       {serverResponses.map((response, index) => {
         return(
           <>
