f09f02899eac830be32de4159e6a96d41cb8d728
clairecassidy <clairecassidy0@hotmail.com>
2020-12-30 19:32:51 +0000

    graph displaying day by day data, need to fix x-axis label

--- client/src/CommitsPerDayGraph.js
+++ client/src/CommitsPerDayGraph.js
@@ -10,6 +10,7 @@ import {
 } from "recharts";
 
 export default function CommitGraph({ commitData }) {
+    
   const [dayData, setDayData] = useState([]);
 
   useEffect(() => {
@@ -35,9 +36,8 @@ export default function CommitGraph({ commitData }) {
 
       console.log(JSON.stringify(dateCountArray));
 
-      // const d1 = new Date('October 1, 2016 12:00:00 GMT+0000');
-      // const d2 = new Date('October 2, 2016 14:00:00 GMT+0000');
-      // console.log(`1. ${d1}\n2. ${d2}\nAre they the same day?: ${sameDay(d1,d2)}`);
+      return dateCountArray;
+    //   setDayData(dateCountArray);
     }
   };
 
@@ -68,53 +68,21 @@ export default function CommitGraph({ commitData }) {
       d1.getDate() === d2.getDate()
     );
   };
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-    },
-  ];
 
   return (
-    <BarChart width={730} height={250} data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
-    </BarChart>
+    <>
+      {console.log(dayData)}
+      {dayData && dayData.length > 0 && (
+        <BarChart width={730} height={250} data={dayData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="count" fill="#8884d8" />
+          {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+        </BarChart>
+      )}
+    </>
   );
 }
